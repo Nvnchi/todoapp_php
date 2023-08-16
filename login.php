f1ff4c3959178ae6d0605512115047449ec5a1c3
Nvnchi <r0879363@student.thomasmore.be>
2023-08-16 15:49:38 +0200

    feat: added html and css to login and register page

--- login.php
+++ login.php
@@ -3,13 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caprasimo&family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <title>Log in</title>
 </head>
 <body>
-    <header>
 
-    </header>
+<div class="custom-shape-divider-bottom-1692189963">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+    </svg>
+</div>
 
-    
+<div id="register">
+    <h1 class="title">Welcome back!</h1>
+    <h2 class="subtitle">Time to get some tasks done! No account yet? Sign up <a href="register.php">here</a> to make one!</h2>
+</div>
+
+
+<div class="registerForm">
+ 
+  <form>
+    <div class="input">
+        <label for="username">Username or email</label>
+        <input type="text" id="username" name="username">
+    </div>
+
+    <div class="input">
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password">
+    </div>
+    <a href="homepage.php">Login</a>
+  </form>
+
+</div>
+
 </body>
 </html>
